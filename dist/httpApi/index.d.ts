3e300040a59702a8e17e1da227c5bccae510dff9
jeane paul soliva <jeanepaul@gmail.com>
2020-10-12 02:39:47 +0900

    ignore dist

--- dist/httpApi/index.d.ts
+++
@@ -1,8 +0,0 @@
-export default class HttpAPI {
-    /**
-     * @param  {string} apiUrl
-     * @param  {any} payload
-     * @returns Promise
-     */
-    static Post<T>(apiUrl: string, payload: any): Promise<T>;
-}
--- dist/models/users.d.ts
+++
@@ -1,7 +0,0 @@
-export interface UsersLoginReq {
-    email: string;
-    password: string;
-}
-export interface UsersLoginResp {
-    token: string;
-}
