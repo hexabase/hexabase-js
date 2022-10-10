6babd0377e40d6be8975f7bfab7b6ee43f4336ce
Trần Xuân Huy <86471028+huytxph10571@users.noreply.github.com>
2022-10-10 17:27:03 +0700

    move function getDsFields to datastore, fix deleteStorage

--- src/lib/packages/storage/storage.test.ts
+++ src/lib/packages/storage/storage.test.ts
@@ -6,8 +6,9 @@ require('dotenv').config();
  * @cmdruntest yarn jest src/lib/packages/user/user.test.ts
  */
 
+let getDownloadFileId = process.env.FILE_ID || '';
+let tokenStr = process.env.TOKEN || '';
 const url = process.env.URL || '';
-let tokenUs = process.env.TOKEN || '';
 const confirmationId = process.env.CONFIRMATIONID || '';
 const email = process.env.EMAIL || '';
 const password = process.env.PASSWORD || '';
@@ -18,9 +19,9 @@ beforeAll(async () => {
     const auth = new Auth(url);
     const { token, error } = await auth.login({ email, password });
     if (token) {
-      return tokenUs = token;
+      return tokenStr = token;
     } else {
-      throw Error(`Need login faild to initialize sdk: ${error}`);
+      throw Error(`Need login fail to initialize sdk: ${error}`);
     }
   }
 });
@@ -29,11 +30,25 @@ describe('User', () => {
   describe('#GetDownloadFile()', () => {
     it('should get download file without error', async () => {
       jest.useFakeTimers('legacy');
-      const storage = new Storage(url, tokenUs);
-      const { data, error } = await storage.GetDownloadFile();
+      const storage = new Storage(url, tokenStr);
+      const { getDownloadFile, error } = await storage.GetDownloadFile(getDownloadFileId);
+      // expect response getPasswordExpire
+      if (getDownloadFile) {
+        expect(typeof getDownloadFile).toBe('string');
+      } else {
+        throw new Error(`Error: ${error}`);
+      }
+    });
+  });
+
+  describe('#deleteStorage()', () => {
+    it('should delete file without error', async () => {
+      jest.useFakeTimers('legacy');
+      const storage = new Storage(url, tokenStr);
+      const { data, error } = await storage.deleteStorage(getDownloadFileId);
       // expect response getPasswordExpire
       if (data) {
-        expect(typeof data).toBe('any');
+        expect(typeof data).toBe('object');
       } else {
         throw new Error(`Error: ${error}`);
       }
--- src/lib/types/storage/response.ts
+++ src/lib/types/storage/response.ts
@@ -1,5 +1,16 @@
 import { ResponseOkModel } from '../../util/type';
 
-export interface StorageResp {
+// export data from graphql
+export interface DtStorageResp {
 	deleteStorage: ResponseOkModel;
+}
+
+export interface DtGetDownloadFile {
+	getDownloadFile?: any,
+}
+
+// export data SDK
+export interface GetDownloadFileRes {
+	getDownloadFile?: any,
+	error?: string,
 }
