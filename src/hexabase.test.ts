7ea27c0ce385f9ffab67a8a4305bed5ccdc09d73
Nguyen Duy Ban <BanND@ltsgroup.tech>
2022-10-31 17:41:37 +0700

    update create client optional param

--- src/hexabase.test.ts
+++ src/hexabase.test.ts
@@ -8,45 +8,87 @@ jest.useRealTimers();
  * @cmdruntest yarn jest src/hexabase.test.ts
  */
 
-const url = process.env.URL || '';
-let tokenClient = process.env.TOKEN || '';
+const proUrl = process.env.PROD_URL || '';
+const stagUrl = process.env.STAG_URL || '';
 const email = process.env.EMAIL || '';
 const password = process.env.PASSWORD || '';
+const prodEmail = process.env.EMAIL || '';
+const prodPassword = process.env.PASSWORD || '';
+const stagToken = process.env.STAG_TOKEN || '';
+const prodToken = process.env.PROD_TOKEN || '';
 const workspaceId = process.env.WORKSPACEID || '';
+const tokenClient = process.env.TOKEN || '';
 
-
-beforeAll( async () => {
-  if (email && password) {
-    console.log('email, password', email, password);
-    const auth = new Auth(url);
-    const {token, error} = await auth.login({email, password});
-    if (token) {
-      return tokenClient = token;
-    } else {
-      throw Error(`Need login faild to initialize sdk: ${error}`);
-    }
-  }
-});
+// beforeAll(async () => {
+//   if (email && password) {
+//     console.log("email, password", email, password);
+//     const auth = new Auth(stagUrl);
+//     const { token, error } = await auth.login({ email, password });
+//     if (token) {
+//       return (tokenClient = token);
+//     } else {
+//       throw Error(`Need login faild to initialize sdk: ${error}`);
+//     }
+//   }
+// });
 
 // testing createClient
 describe('Hexabase', () => {
   describe('#createClient()', () => {
-    it('application: get createClient and testing', async () => {
-      jest.useFakeTimers('legacy');
-      // const hexabase = await createClient({ url, email, password });
-      const hexabase = await createClient({ url, token: tokenClient });
-
-      console.log('Test: class application');
-      const {appAndDs, error} = await hexabase.applications.getProjectsAndDatastores(workspaceId);
-      if (appAndDs && appAndDs[0]) {
-
-        // console.log('appAndDs', appAndDs);
-        expect(typeof appAndDs[0].name).toBe('string');
-        expect(typeof appAndDs[0].application_id).toBe('string');
-        expect(typeof appAndDs[0].display_id).toBe('string');
+    it('get createClient and testing with url, email, password', async () => {
+      jest.useFakeTimers();
+      // with 3 params: base url, user's email and password
+      console.log('stagUrl', stagUrl);
+      const hexabase = await createClient({
+        url: stagUrl,
+        email,
+        password,
+      });
+      const { wsCurrent, error } = await hexabase.workspaces.getCurrent();
+      if (wsCurrent) {
+        expect(typeof wsCurrent.workspace_id).toBe('string');
       }
     });
   });
+
+  describe('#createClient()', () => {
+    it('get createClient and testing with url and token', async () => {
+      jest.useFakeTimers();
+      const hexabase = await createClient({ url: stagUrl, token: stagToken });
+      const { wsCurrent, error } = await hexabase.workspaces.getCurrent();
+      if (wsCurrent) {
+        expect(typeof wsCurrent.workspace_id).toBe('string');
+      }
+    });
+  });
+
+  describe('#createClient()', () => {
+    it('get createClient and testing with email, password', async () => {
+      jest.useFakeTimers();
+      const hexabase = await createClient({
+        email: prodEmail,
+        password: prodPassword,
+      });
+      const { wsCurrent, error } = await hexabase.workspaces.getCurrent();
+      if (wsCurrent) {
+        expect(typeof wsCurrent.workspace_id).toBe('string');
+      }
+    });
+  });
+  describe('#createClient()', () => {
+    it('get createClient and testing with just token', async () => {
+      jest.useFakeTimers();
+      // with 2 params: only user's email and password (without base url)
+      const hexabase = await createClient({
+        token: prodToken,
+      });
+      const { wsCurrent, error } = await hexabase.workspaces.getCurrent();
+      if (wsCurrent) {
+        expect(typeof wsCurrent.workspace_id).toBe('string');
+      }
+    });
+  });
+
   // describe('#getFile()', () => {
   //   it('application: get createClient and testing', async () => {
   //     jest.useFakeTimers('legacy');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import HexabaseClient from './HexabaseClient';
 import Auth from './lib/packages/auth';
 import AuthMw from './lib/packages/middlware/auth';
-
+require('dotenv').config();
 interface HexabaseConfig {
-  url: string;
+  url?: string;
   token?: string;
   email?: string;
   password?: string;
@@ -12,28 +12,29 @@ interface HexabaseConfig {
 /**
  * create client for hexabase-sdk
  */
-const createClient = async ({ url, token, email, password }: HexabaseConfig): Promise<HexabaseClient> => {
-  const auth = new Auth(url);
+const createClient = async ({
+  url,
+  token,
+  email,
+  password,
+}: HexabaseConfig): Promise<HexabaseClient> => {
+  const getUrl = url || process.env.PROD_URL!;
+  const auth = new Auth(getUrl);
   let tokenHx = '';
 
   if (email && password && !token) {
-    const {token, error} = await auth.login({email, password});
+    const { token, error } = await auth.login({ email, password });
     if (token) {
       tokenHx = token;
     } else {
       throw Error(`Need login faild to initialize sdk: ${error}`);
     }
-  }
-  else if (!email && !password && token) {
+  } else if (!email && !password && token) {
     tokenHx = token;
-  }
-  else {
+  } else {
     throw Error('Need token or email and password to initialize sdk');
   }
-    return new HexabaseClient(url, tokenHx);
+  return new HexabaseClient(getUrl, tokenHx);
 };
 
-export {
-  createClient,
-  HexabaseClient,
-};
+export { createClient, HexabaseClient };
