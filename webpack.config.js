e314819531de398efed18d02679fb3c293872ee1
kondokentaro <kondokentaro@gmail.com>
2023-10-23 10:13:06 +0900

    add umd setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, 'dist/'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist/umd/'),
+    filename: 'hexabase.js',
     library: {
-      type: 'hxb',
-      name: 'index',
+      type: 'umd',
+      name: 'hexabase',
     },
   },
   module: {
