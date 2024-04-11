e8064dd3286d7e0ceafe2b69ce34319b307a42a6
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2024-04-11 18:38:35 +0900

    Support LocalStorage, SessionStorage, Cookie

--- src/lib/packages/auth/auth.test.ts
+++ src/lib/packages/auth/auth.test.ts
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
+import exp from 'constants';
 import Auth from '.';
 import { HexabaseClient } from '../../../';
+import { LocalStorage } from 'node-localstorage';
+
+const localStorage = new LocalStorage('./scratch');
 
 const email = process.env.EMAIL || '';
 const password = process.env.PASSWORD || '';
@@ -10,20 +14,31 @@ const client = new HexabaseClient();
 describe('Auth', () => {
   describe('login function', () => {
     it('should login by email and password', async () => {
-      const auth = new Auth(client.urlHxb);
-      const result = await auth.login({ email, password });
-      expect(typeof result).toBe('string');
+      const result = await client.login({ email, password });
+      expect(typeof result).toBe('boolean');
     });
   });
 
   describe('logout function', () => {
     it('should logout', async () => {
-      const auth = new Auth(client.urlHxb);
-      const resultLogin = await auth.login({ email, password });
-      expect(typeof resultLogin).toBe('string');
-      auth.client.setHeaders({ authorization: `Bearer ${resultLogin}` });
-      const resultLogout = await auth.logout();
-      expect(resultLogout).toBe(true);
+      const res = await client.login({ email, password });
+      expect(typeof res).toBe('boolean');
+      const res2 = await client.logout();
+      expect(res2).toBe(true);
+      expect(client.currentUser).toBe(undefined);
+    });
+  });
+
+  describe('Persistence authentication status', () => {
+    it('should persistence', async () => {
+      client.setPersistence(localStorage);
+      const resultLogin = await client.login({ email, password });
+      expect(typeof resultLogin).toBe('boolean');
+      expect(localStorage.length).toBe(1);
+      expect(localStorage.getItem(client.auth.tokenName)).not.toBe(null);
+      await client.logout();
+      expect(localStorage.length).toBe(0);
+      expect(client.currentUser).toBe(undefined);
     });
   });
 
